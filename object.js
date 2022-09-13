1853e0f2271e8dc2f581ae38de90097665bec3a4
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-13 16:57:20 -0600

    starting practice work

--- object.js
+++ object.js
@@ -1,12 +1,35 @@
-const userOne = {
-  email: "bob@bob.com",
-  name: "Bob",
-  login() {
-    console.log(this.email, "Has logged in")
-  },
-  logout() {
-    console.log(this.email, "Has logged out")
-  }
-};
+// const userOne = {
+//   email: "bob@bob.com",
+//   name: "Bob",
+//   login() {
+//     console.log(this.email, "Has logged in")
+//   },
+//   logout() {
+//     console.log(this.email, "Has logged out")
+//   }
+// };
 
-console.log(userOne.name);
+// console.log(userOne.name);
+
+/**
+ * Create a Backpack object.
+ */
+
+ const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+};
