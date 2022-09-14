62971a6f94af95e4ae09759a693feeeaffeb7a3d
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-14 13:07:18 -0600

    git status working again and llearning to manipulate objects

--- golfBag.js
+++ golfBag.js
@@ -8,5 +8,38 @@ const golfBag = {
   ballBrand: "Callaway",
 };
 
+const markup = (bag) => {
+  return `
+    <div>
+      <h3>Club Brand: ${bag.clubBrand}</h3>
+      <h3>Club List:</h3>
+      <ul>
+        <li>${bag.clubList[0]}</li>
+        <li>${bag.clubList[1]}</li>
+        <li>${bag.clubList[2]}</li>
+        <li>${bag.clubList[3]}</li>
+        <li>${bag.clubList[4]}</li>
+        <li>${bag.clubList[5]}</li>
+        <li>${bag.clubList[6]}</li>
+        <li>${bag.clubList[7]}</li>
+        <li>${bag.clubList[8]}</li>
+        <li>${bag.clubList[9]}</li>
+        <li>${bag.clubList[10]}</li>
+        <li>${bag.clubList[11]}</li>
+        <li>${bag.clubList[12]}</li>
+        <li>${bag.clubList[13]}</li>
+      </ul>
+      <h3>Ball Brand: ${bag.ballBrand}</h3>
+    </div>
+
+
+  `;
+};
+
 console.log(golfBag);
 console.log(golfBag.clubList[1]);
+
+
+const main = document.createElement("main");
+main.innerHTML = markup(golfBag);
+document.body.appendChild(main);
