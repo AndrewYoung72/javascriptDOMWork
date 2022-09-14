8c96bf37d933580ad55ca74289441f34199c50b1
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-14 12:45:21 -0600

    building objects to tinker with

---
+++ golfBag.js
@@ -0,0 +1,12 @@
+const golfBag = {
+  clubNumber: 14,
+  clubList: ["Driver", "3H", "3 Iron", "4 Iron", "5 Iron", "6 Iron","7 Iron","8 Iron","9 Iron","PW", "50°W", "54°W", "58°W", "Putter"],
+  clubBrand: "Callaway",
+  golfTowel: true,
+  tees: true,
+  balls: true,
+  ballBrand: "Callaway",
+};
+
+console.log(golfBag);
+console.log(golfBag.clubList[1]);
