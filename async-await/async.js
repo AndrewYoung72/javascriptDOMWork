77e4fb2b23ce1eb7760ce492dc91dcef8d278eef
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-16 10:52:25 -0600

    taking a break, style nad text content null issues

---
+++ async-await/async.js
@@ -0,0 +1,7 @@
+// Callback functions
+setTimeout(function() {
+  const banner = document.getElementById("#banner");
+  // banner.style.backgroundColor = "blue";
+  banner.textContent = "It worked!"
+}, 5000);//5 second delay /5 thousnd milliseconds
+
