0ce8f4d7f7179af3a45e5b41d5790724e4727cb7
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-17 10:54:52 -0600

    closer to working errors fixed but text not rendering

--- async-await/async.js
+++ async-await/async.js
@@ -3,15 +3,15 @@ setTimeout(function () {
   const banner = document.getElementById("banner");
   banner.style.backgroundColor = "blue";
   console.log("Hello there!");
-}, 5000); //5 second delay toggles background color to blue
+}, 2000); //5 second delay toggles background color to blue
 
 function flipText() {
-  const flip = document.getElementById("banner").innerHTML;
+  document.getElementById("button").addEventListener("click", flipText, true);
+  const flip = document.getElementById("text").textContent;
   console.log(flip);
-  if (flip.innerHTML === "<h1>Async Await Practice</h1>") {
-    flip.innerHTML === "<h1>Funtional Programming!</h1>";
+  if (flip === "Async Await Practice") {
+    flip === "Funtional Programming!";
   } else {
-    flip.innerHTML === "<h1>Async Await Practice</h1>";
+    flip === "Async Await Practice";
   }
-  document.getElementById("button").addEventListener("click", flipText)
 }
