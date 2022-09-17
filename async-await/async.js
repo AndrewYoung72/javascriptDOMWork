06184010333b52cacba81a5cff507dc22a750ce7
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-17 10:11:03 -0600

    function for flipping text not working yet

--- async-await/async.js
+++ async-await/async.js
@@ -1,7 +1,17 @@
-// Callback functions
-setTimeout(function() {
-  const banner = document.getElementById("#banner");
-  // banner.style.backgroundColor = "blue";
-  banner.textContent = "It worked!"
-}, 5000);//5 second delay /5 thousnd milliseconds
+// Callback functions/setTimeout
+setTimeout(function () {
+  const banner = document.getElementById("banner");
+  banner.style.backgroundColor = "blue";
+  console.log("Hello there!");
+}, 5000); //5 second delay toggles background color to blue
 
+function flipText() {
+  const flip = document.getElementById("banner").innerHTML;
+  console.log(flip);
+  if (flip.innerHTML === "<h1>Async Await Practice</h1>") {
+    flip.innerHTML === "<h1>Funtional Programming!</h1>";
+  } else {
+    flip.innerHTML === "<h1>Async Await Practice</h1>";
+  }
+  document.getElementById("button").addEventListener("click", flipText)
+}
