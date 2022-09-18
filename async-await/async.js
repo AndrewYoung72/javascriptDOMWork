b91424f948d9e94473e1a0901b839886fed6a6ec
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-18 08:25:33 -0600

    clearing commits

--- async-await/async.js
+++ async-await/async.js
@@ -1,17 +1,18 @@
 // Callback functions/setTimeout
-setTimeout(function () {
-  const banner = document.getElementById("banner");
-  banner.style.backgroundColor = "blue";
-  console.log("Hello there!");
-}, 2000); //5 second delay toggles background color to blue
+// setTimeout(function () {
+//   const banner = document.getElementById("banner");
+//   banner.style.backgroundColor = "blue";
+//   // console.log("Hello there!");
+// }, 2000); //5 second delay toggles background color to blue
 
-function flipText() {
-  document.getElementById("button").addEventListener("click", flipText, true);
+ function flipText() {
   const flip = document.getElementById("text").textContent;
+  const newText = document.getElementById("alt");
   console.log(flip);
   if (flip === "Async Await Practice") {
-    flip === "Funtional Programming!";
+     newText === "Funtional Programming!";
   } else {
-    flip === "Async Await Practice";
+     newText === "Async Await Practice";
   }
+  document.getElementById("button").addEventListener("click", flipText, true)
 }
